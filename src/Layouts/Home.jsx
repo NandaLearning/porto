8764b6d42dedf6682d5c5e07da137458f2f39211
nanda <nanda@gmail.com>
2024-01-22 02:00:09 -0800

    first commit

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
 import CardDev from '../Components/cardDev/CardDev';
 
 const Home = () => {
   return (
-    <div className='p-10 mt-10 space-x-10'>
-      <h1 className='text-white font-bold text-2xl font-sans md:text-4xl'>
-        Welcome to the world of digital creativity!<br></br>I am{' '}
-        <Link to='/' className='text-purple-700 font-bold'>
-          Ananda Lukman Ramadhan,
-        </Link>
-        <br />a dedicated Fullstack Web Developer.
-      </h1>
-      <div className='grid justify-center mt-16 mx-20 md:grid-cols-2 md:gap-10'>
+    <div className='p-10 mt-10 space-y-10 md:space-y-0 md:flex md:items-center'>
+      <div className='md:w-1/2 md:mr-10'>
+        <h1 className='text-white font-bold text-2xl font-sans md:text-4xl'>
+          Welcome to the world of digital creativity!<br />
+          I am{' '}
+          <Link to='/' className='text-purple-700 font-bold'>
+            Ananda Lukman Ramadhan,
+          </Link>
+          <br />a dedicated Fullstack Web Developer.
+        </h1>
 
+        <h1 className='font-bold md:mt-10 text-2xl text-white'>Biography</h1>
+        <p className='text-white text-xl mt-4'>
+          Ananda Lukman Ramadhan is a skilled Fullstack Web Developer with a
+          year-long journey in the world of programming. His dedication to
+          mastering the art of web development has been evident throughout his
+          learning journey.
+        </p>
+
+        <div className='mt-7'>
+          <h1 className='font-bold text-2xl text-white'>Let's Connect:</h1>
+          <div className='flex mt-3 space-x-5'>
+            <img src='facebook.svg' alt='Facebook' />
+            <img src='instagram.svg' alt='Instagram' />
+            <img src='twitter.svg' alt='Twitter' />
+          </div>
+        </div>
+      </div>
+
+      <div className='md:w-1/2 md:ml-10'>
         <div className='flex items-center justify-center'>
           <img
             src='profile.png'
@@ -22,26 +41,7 @@ const Home = () => {
             alt='Profile'
           />
         </div>
-
-        <div>
-          <h1 className='font-bold md:mt-0 mt-10 text-2xl text-white md:mb-4'>Biography</h1>
-          <p className='text-white text-xl mt-4'>
-            Ananda Lukman Ramadhan is a skilled Fullstack Web Developer with a
-            year-long journey in the world of programming. His dedication to
-            mastering the art of web development has been evident throughout
-            his learning journey.
-          </p>
-        <div className=' mt-7'>
-          <h1 className='font-bold text-2xl text-white'>Lets Connect :</h1>
-          <div className='flex mt-10 space-x-5'>
-            <img src='facebook.svg' alt='Facebook' />
-            <img src='instagram.svg' alt='Instagram' />
-            <img src='twitter.svg' alt='Twitter' />
-          </div>
-        </div>
       </div>
-        </div>
-
 
       <div className='mt-14'>
         <h1 className='text-3xl font-bold text-white'>What I Do</h1>
@@ -49,11 +49,11 @@ const Home = () => {
           I am not only a Fullstack Web Developer but also have been learning
           various other skills such as app development, UI/UX, and DevOps.
         </p>
-        <div className='flex justify-center flex-wrap md:space-x-10'>
-          <CardDev img='app.svg' title='App Developer' />
-          <CardDev img='copy.svg' title='UI/UX'  />
-          <CardDev img='devops.svg' title='DevOps'  />
 
+        <div className='flex justify-center flex-wrap mt-4 md:space-x-10'>
+          <CardDev img='app.svg' title='App Developer' />
+          <CardDev img='copy.svg' title='UI/UX' />
+          <CardDev img='devops.svg' title='DevOps' />
         </div>
       </div>
     </div>
