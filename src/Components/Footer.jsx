cf9757d8caa5c2bad784ce495140dc3730361389
nanda <nanda@gmail.com>
2024-01-23 02:22:19 -0800

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-white space-y-6 bg-gray-800 w-full mt-64 h-72'>
-      <h1 className=' text-2xl text-center'>Ananda Lukman Ramadhan</h1>
-      <ul className=' flex justify-center space-x-8'>
+      <h1 className='text-2xl text-center'>Ananda Lukman Ramadhan</h1>
+      <ul className=' flex justify-center space-x-3 md:space-x-8'>
         <li>Home</li>
         <li>Technology</li>
         <li>Project</li>
         <li>Bussines</li>
-        <li>Contact</li>
       </ul>
       <div className=' flex justify-center items-center space-x-5 mt-4'>
         <img src='facebook.svg'></img>
